5c521f55d1ebba76ecf0e698eeca7aaf4ef26171
oli <oli@qq.com>
2020-12-22 16:47:20 +0800

    搜索资产

--- pages/assets/index.vue
+++ pages/assets/index.vue
@@ -1,5 +1,44 @@
 <template>
   <section>
+    <a-form :form='form' layout='inline' @submit='handleSubmit'>
+      <a-form-item label='条件'>
+        <a-select
+          style='width: 10em;'
+          v-decorator="[
+          'select',
+          { rules: [{ required: true, message: 'Please select your select!' }], initialValue: 'snID' },
+        ]"
+          placeholder='选择查找的字段'
+        >
+          <a-select-option value='snID'>
+            资产编号
+          </a-select-option>
+          <a-select-option value='name'>
+            中文姓名
+          </a-select-option>
+          <a-select-option value='department'>
+            部门
+          </a-select-option>
+          <a-select-option value='ip_address'>
+            IP地址
+          </a-select-option>
+          <a-select-option value='action'>
+            状态
+          </a-select-option>
+        </a-select>
+      </a-form-item>
+      <a-form-item label='关键字'>
+        <a-input
+          v-decorator="['keyword']"
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type='primary' html-type='submit'>
+          查找
+        </a-button>
+      </a-form-item>
+    </a-form>
+    <a-divider />
     <a-table bordered :columns="assetsColumns" :data-source="assets" rowKey="id" :pagination="pagination">
       <template slot="name" slot-scope="text, n">
         {{n.user_info.name}} - {{n.user_info.name_en}}
@@ -57,7 +96,8 @@
             key: 'action',
             scopedSlots: { customRender: 'action' }
           }
-        ]
+        ],
+        form: this.$form.createForm(this, { name: 'coordinated' })
       }
     },
     async created() {
@@ -65,9 +105,23 @@
       this.assets = result.data
     },
     methods: {
-      action() {
-
-      }
+      handleSubmit(e) {
+        e.preventDefault()
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            // console.log('Received values of form: ', values);
+            this.searchUser(values)
+          }
+        })
+      },
+      async searchUser(select) {
+        // console.log(select)
+        let result = await this.$axios.$get(this.$store.state.api.getAssets, {
+          params: select
+        })
+        // console.log(result)
+        this.assets = result.result.data
+      },
     }
   }
 </script>
